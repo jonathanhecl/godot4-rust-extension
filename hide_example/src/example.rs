55853a808fd01fe99cfdfdd28140ccb7683b9be2
Jonathan Hecl <jonathanhecl@gmail.com>
2023-11-28 01:48:40 -0300

    func & signal test

---
+++ hide_example/src/example.rs
@@ -0,0 +1,37 @@
+use godot::prelude::*;
+
+use godot::engine::INode2D;
+
+#[derive(GodotClass)]
+#[class(base=Node2D)]
+
+struct HideExample {
+
+    #[base]
+    node2d: Base<Node2D>
+}
+
+
+#[godot_api]
+impl INode2D for HideExample {
+    fn init(node2d: Base<Node2D>) -> Self {
+        godot_print!("HideExampleNode is initialized!");
+        Self { node2d }
+    }
+
+    fn process(&mut self, delta: f64) {
+        //godot_print!("HideExampleNode is processing!");
+    }
+}
+
+#[godot_api]
+impl HideExample {
+    #[func]
+    fn hello_world(&mut self, name:GodotString) {
+        godot_print!("Hello world! {name}");
+        self.node2d.emit_signal("hello_world_signal".into(), &[]);
+    }
+
+    #[signal]
+    fn hello_world_signal();
+}
--- hide_example/src/lib.rs
+++ hide_example/src/lib.rs
@@ -5,27 +5,4 @@ struct HideExampleExtension;
 #[gdextension]
 unsafe impl ExtensionLibrary for HideExampleExtension {}
 
-use godot::engine::Node2D;
-
-#[derive(GodotClass)]
-#[class(base=Node2D)]
-
-struct HideExample {
-
-    #[base]
-    node2d: Base<Node2D>
-}
-
-use godot::engine::INode2D;
-
-#[godot_api]
-impl INode2D for HideExample {
-    fn init(node2d: Base<Node2D>) -> Self {
-        godot_print!("HideExampleNode is initialized!");
-        Self { node2d }
-    }
-
-    fn process(&mut self, delta: f64) {
-        godot_print!("HideExampleNode is processing!");
-    }
-}
+mod example;
