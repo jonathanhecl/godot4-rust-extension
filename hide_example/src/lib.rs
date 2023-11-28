3c6ebd481102af5c38989bde3234a0aff6111019
Jonathan Hecl <jonathanhecl@gmail.com>
2023-11-28 01:24:55 -0300

    works!

--- hide_example/src/lib.rs
+++ hide_example/src/lib.rs
@@ -9,16 +9,17 @@ use godot::engine::Node2D;
 
 #[derive(GodotClass)]
 #[class(base=Node2D)]
+
 struct HideExample {
 
     #[base]
     node2d: Base<Node2D>
 }
 
-use godot::engine::Node2DVirtual;
+use godot::engine::INode2D;
 
 #[godot_api]
-impl Node2DVirtual for HideExample {
+impl INode2D for HideExample {
     fn init(node2d: Base<Node2D>) -> Self {
         godot_print!("HideExampleNode is initialized!");
         Self { node2d }
