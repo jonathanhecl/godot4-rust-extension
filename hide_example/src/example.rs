6b7ee5e05418e1a96c7bd38ca0ebd98d8f2ce817
Jonathan Hecl <jonathanhecl@gmail.com>
2023-12-14 00:26:57 -0300

    open_file

--- hide_example/src/example.rs
+++ hide_example/src/example.rs
@@ -1,6 +1,9 @@
+use std::fs;
+
 use godot::prelude::*;
 
 use godot::engine::INode2D;
+use std::path::Path;
 
 #[derive(GodotClass)]
 #[class(base=Node2D)]
@@ -19,9 +22,10 @@ impl INode2D for HideExample {
         Self { node2d }
     }
 
-    //fn process(&mut self, delta: f64) {
+    fn process(&mut self, delta: f64) {
         //godot_print!("HideExampleNode is processing!");
-    //}
+        self.node2d.rotate((5f64 * delta) as f32);
+    }
 }
 
 #[godot_api]
@@ -30,6 +34,31 @@ impl HideExample {
     fn hello_world(&mut self, name:GString) {
         godot_print!("Hello world! {name}");
         self.node2d.emit_signal("hello_world_signal".into(), &[]);
+    }
+
+    #[func]
+    fn load_map_data(&mut self, path: GString) {
+        godot_print!("Loading map data from {path}");
+    }
+
+    // open file like a text and return the content
+    #[func]
+    fn open_file(&mut self, path: GString) -> GString {
+        godot_print!("Opening file {}", path);
+        let path_str = path.to_string(); // Convert GString to String
+        let path = Path::new(&path_str);
+        let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
+        let contents = GString::from(contents);
+        return contents;        
+    }
+
+
+    #[func]
+    fn fibonacci(&mut self, n: i64) -> i64 {
+        if n <= 1 {
+            return n;
+        }
+        return self.fibonacci(n - 2) + self.fibonacci(n - 1);
     }
 
     #[signal]
